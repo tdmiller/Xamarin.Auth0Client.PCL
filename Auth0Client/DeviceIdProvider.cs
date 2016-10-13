090218d4280f1a2549e605ae27405f98711c060f
Tom Miller <tom@shepardroad.com>
2016-10-13 16:04:57 -0500

    Consolidated Into Single PCL Forms Project
    Started piecing together required parts for a single PCL Xamarin.Forms
    library to serve up Auth0 integration.

---
+++ Auth0Client/DeviceIdProvider.cs
@@ -0,0 +1,14 @@
+﻿using Plugin.DeviceInfo;
+using System.Threading.Tasks;
+
+namespace Auth0.SDK
+{
+	public class DeviceIdProvider : IDeviceIdProvider
+	{
+		public Task<string> GetDeviceId ()
+		{
+			return Task.FromResult<string>(CrossDeviceInfo.Current.Id);
+		}
+	}
+}
+
